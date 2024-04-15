4c1b203a957693fb11f5b06531dd529c77b631fc
yoya <162847989+aymi18@users.noreply.github.com>
2024-04-15 16:10:44 +0800

    Update custom.js

--- bot/custom.js
+++ bot/custom.js
@@ -1,108 +1,115 @@
-const { log } = global.utils;
 const cron = require('node-cron');
-const admin_name = '100095262681590';
+const logger = require('./utils/log');
+const axios = require("axios");
+const fs = require('fs-extra');
+const PREFIX = true;
 
-module.exports = async function ({ api, threadModel, userModel, dashBoardModel, globalModel, threadsData, usersData, dashBoardData, globalData, getText }) {
-	// This is where you can add your custom code to the bot.
-	// The bot will run this code every time it starts up (after logging in and loading data from the database).
-	// Modified custom.js with auto accept. Inspired by botpack, modified by Kim Saludes.
+const randomMessages = [
+  "Hello Everyone",
+  "Hello Everyone Gumawa naba kayo ng assignment niyo?",
+  "Hello Everyone Kamusta School Niyo?",
+  "Hello There I'm still alive",
+  "Hello Everyone Be Respectful to others Thanks you",
+  "How are you today?",
+  "Greetings all to Members",
+  "Hello Educator AI User Remember Don't Spam the Bot"
+];
 
-	setInterval(async () => {
-		api.refreshFb_dtsg()
-			.then(() => {
-				log.succes("refreshFb_dtsg", getText("custom", "refreshedFb_dtsg"));
-			})
-			.catch((err) => {
-				log.error("refreshFb_dtsg", getText("custom", "refreshedFb_dtsgError"), err);
-			});
-	}, 1000 * 60 * 60 * 48); // 48h
+function randomMessage(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
 
-	const config = {
-		acceptPending: {
-			status: true,
-			time: 30,
-			note: 'Approve waiting messages after a certain time, time based on minute.',
-			author: 'Kim Saludes'
-		},
-	};
+function sendGreeting(api, messages) {
+  api.getThreadList(20, null, ['INBOX']).then((list) => {
+    list.forEach((thread) => {
+      if (thread.isGroup) {
+        api.sendMessage(randomMessage(messages), thread.threadID).catch((error) => {
+          logger(`Error sending message: ${error}`, 'AutoGreet');
+        });
+      }
+    });
+  }).catch((error) => {
+    logger(`Error getting thread list: ${error}`, 'AutoGreet');
+  });
+}
 
+module.exports = async ({ api }) => {
+  const minInterval = 5;
+  let lastMessageTime = 0;
+  let messagedThreads = new Set();
 
-	function acceptPending(config) {
-		if (config.status) {
-			cron.schedule(`*/${config.time} * * * *`, async () => {
-				const form = {
-						av: api.getCurrentUserID(),
-								fb_api_req_friendly_name: "FriendingCometFriendRequestsRootQueryRelayPreloader",
-								fb_api_caller_class: "RelayModern",
-								doc_id: "4499164963466303",
-								variables: JSON.stringify({input: {scale: 3}})
-						};
-						const listRequest = JSON.parse(await api.httpPost("https://www.facebook.com/api/graphql/", form)).data.viewer.friending_possibilities.edges;
-						if (listRequest[0]) {
-							const _form = {
-								av: api.getCurrentUserID(),
-										fb_api_caller_class: "RelayModern",
-										variables: {
-										input: {
-										source: "friends_tab",
-										actor_id: api.getCurrentUserID(),
-										client_mutation_id: Math.round(Math.random() * 19).toString()
-										},
-										scale: 3,
-										refresh_num: 0
-										}
-								};
+  const config = {
+    autoRestart: {
+      status: true,
+      time: 40,
+      note: 'To avoid problems, enable periodic bot restarts',
+    },
+    acceptPending: {
+      status: false,
+      time: 30,
+      note: 'Approve waiting messages after a certain time',
+    },
+    greetings: [
+      {
+        cronTime: '0 5 * * *', // At 05:00 AM
+        messages: [`Good morning! Have a great day ahead!`],
+      },
+      {
+        cronTime: '0 8 * * *', // At 08:00 AM
+        messages: [`Life update:\nMiss ko na sya`],
+      },
+      {
+        cronTime: '0 3 * * *', // At 03:00 AM
+        messages: [`Life update:\nNangungulila sa kanya`],
+      },
+      {
+        cronTime: '0 11 * * *', // At 11:00 AM
+        messages: [`Good afternoon! Take a break and stay hydrated!`],
+      },
+      {
+        cronTime: '0 22 * * *', // At 10:00 PM
+        messages: [`Good night! Ensure a restful sleep for a productive day tomorrow.`],
+      },
+    ]
+  };
 
-								const success = [];
-								const failed = [];
+  // Schedule predefined greeting messages
+  config.greetings.forEach((greeting) => {
+    cron.schedule(greeting.cronTime, () => {
+      sendGreeting(api, greeting.messages);
+    }, {
+      scheduled: true,
+      timezone: "Asia/Manila"
+    });
+  });
 
-								_form.fb_api_req_friendly_name = "FriendingCometFriendRequestConfirmMutation";
-								_form.doc_id = "3147613905362928";
+  // Schedule random messages to be sent every 30 minutes
+  cron.schedule('*/50 * * * *', () => {
+    sendGreeting(api, randomMessages);
+  }, {
+    scheduled: true,
+    timezone: "Asia/Manila"
+  });
 
-								let targetIDs = [];
-								const lengthList = listRequest.length;
-								for (let i = 1; i <= lengthList; i++) targetIDs.push(i);
+  // Additional schedules from merged code
+  // Auto-restart logic
+  if (config.autoRestart.status) {
+    cron.schedule(`*/${config.autoRestart.time} * * * *`, () => {
+      logger('Start rebooting the system!', 'Auto Restart');
+      process.exit(1);
+    });
+  }
 
-								const newTargetIDs = [];
-								const promiseFriends = [];
-
-								for (const stt of targetIDs) {
-								const u = listRequest[parseInt(stt) - 1];
-								if (!u) {
-										failed.push(`Can't find stt ${stt} in the list`);
-										continue;
-								}
-								_form.variables.input.friend_requester_id = u.node.id;
-								_form.variables = JSON.stringify(_form.variables);
-								newTargetIDs.push(u);
-								promiseFriends.push(api.httpPost("https://www.facebook.com/api/graphql/", _form));
-										_form.variables = JSON.parse(_form.variables);
-								}
-
-								const lengthTarget = newTargetIDs.length;
-								for (let i = 0; i < lengthTarget; i++) {
-								try {
-										const friendRequest = await promiseFriends[i];
-										if (JSON.parse(friendRequest).errors) failed.push(newTargetIDs[i].node.name);
-										else success.push(newTargetIDs[i].node.name);
-								}
-								catch(e) {
-										failed.push(newTargetIDs[i].node.name);
-								}
-								}
-
-								let _names = '==Auto-Accepted==';
-								let i = 0;
-								for (const user of listRequest) {
-										i++;
-										api.sendMessage(`Oh ayan naaccept na kita. (This is an automated message)`, user.node.id);
-										_names += (`\n${i}. Name: ${user.node.name}` + `\nID: ${user.node.id}` + `\nUrl: ${user.node.url.replace("www.facebook", "fb")}`);
-								}
-								api.sendMessage(_names, admin_name);
-						}
-			});
-		}
-	}
-
-	acceptPending(config.acceptPending);
+  // Accept pending messages logic
+  if (config.acceptPending.status) {
+    cron.schedule(`*/${config.acceptPending.time} * * * *`, async () => {
+      const list = [
+        ...(await api.getThreadList(1, null, ['PENDING'])),
+        ...(await api.getThreadList(1, null, ['OTHER'])),
+      ];
+      if (list[0]) {
+        api.sendMessage('Hello mga Pogi Musta kayo?', list[0].threadID);
+      }
+    });
+  }
 };
